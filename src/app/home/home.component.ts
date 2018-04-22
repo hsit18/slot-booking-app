1731b020e08f096d4699c43815d2c502847b7d4a
Harpreet Singh <harpreet.singh@globant.com>
2018-04-22 15:02:01 +0530

    styling in progress

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,6 +5,8 @@ import { Router } from '@angular/router';
 
 import { AppService } from '../app.service';
 
+import { Slot } from '../interfaces/slots';
+
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
@@ -14,26 +16,43 @@ export class HomeComponent implements OnInit {
     public days: number[] = getDaysForCurrentMonth();
     public today: number = getTodayDate();
 
+    slots: Slot[] = [];
+    bookedSlots = [];
     constructor(
         private router: Router,
         private appService: AppService
     ) { }
 
     public ngOnInit(): void {
-        // console.log(this.today);
+        this.bookedSlots = [
+            {
+              "title": "test title",
+              "slot_id": 1,
+              "day":22,
+              "month": 4,
+              "year": 2018,
+              "startTime": "12:30",
+              "endTime": "13:30",
+              "comments": "test description"
+            }
+        ];
 
         this.appService
             .getSlots()
             .subscribe(
                 (slots) => {
                     console.log(slots);
-                    //this.slots = slots;
+                    this.slots = slots;
                 }
             );
     }
 
     public handleGridClick(date: number): void {
         this.router.navigate(['book-slot']);
+    }
+
+    public getbookedSlot(day) {
+        return this.bookedSlots.filter(slot => slot.day === day) || [];
     }
 
 }
